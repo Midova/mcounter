473a0a828e623c8c55c760a5bdccb725a451bba6
Midova <darya.demidova2011@gmail.com>
2016-12-03 21:39:40 +0300

    Отредактировада пространство имен в ISession

--- MoneyCounter/MoneyCounter/ISession.cs
+++ MoneyCounter/MoneyCounter/ISession.cs
@@ -1,10 +1,6 @@
 ﻿using MoneyCounter.Data.Model;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MoneyCounter
 {
