011353be5485826b59374562ba757ceed7d6aad5
Midova <darya.demidova2011@gmail.com>
2016-12-04 22:28:09 +0300

    Реализовала Load и Save в классе Session

--- MoneyCounter/MoneyCounter/Session.cs
+++ MoneyCounter/MoneyCounter/Session.cs
@@ -2,6 +2,9 @@
 using System.Collections.ObjectModel;
 using System.Linq;
 using MoneyCounter.Data.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.IO;
 
 namespace MoneyCounter
 {
@@ -38,20 +41,38 @@ namespace MoneyCounter
 		/// <summary>
 		/// Выполняет дисериализацию сессии из указанного файла.
 		/// </summary>
-		/// <param name="filePath"></param>
-		/// <returns></returns>
+		/// <param name="filePath">путь к файлу</param>
+		/// <returns>сессия</returns>
 		public static Session Load(string filePath)
-		{
-						
+		{			
+			if (!File.Exists(filePath))
+				return null;
+
+			Session result = null;
+			using (var reader = File.OpenRead(filePath))
+				result = JsonConvert.DeserializeObject<Session>(reader.ToString());		
+			
+			return result;
 		}
 
 		/// <summary>
-		/// Выполняет сериализацию сессии по указанному пути
+		/// Выполняет сериализацию сессии по указанному пути.
 		/// </summary>
-		/// <param name="filePath"></param>
-		public static void Save(string filePath)
-		{
+		/// <param name="filePath">путь к файлу</param>
+		/// <param name="session">нынешняя сессия</param>
+		public static void Save(string filePath, Session session)
+		{			
+			JsonSerializer serializer = new JsonSerializer();
+			serializer.Converters.Add(new JavaScriptDateTimeConverter());
 
+			serializer.NullValueHandling = NullValueHandling.Ignore;
+
+			using (StreamWriter sw = new StreamWriter(filePath))
+			using (JsonWriter writer = new JsonTextWriter(sw))
+			{
+				serializer.Serialize(writer, session);
+				writer.Close();
+			}
 		}
 	}
 }
