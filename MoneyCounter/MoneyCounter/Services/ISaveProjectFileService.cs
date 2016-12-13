f1577057a234ddad2eff342c2e1d5b129b36c617
Midova <darya.demidova2011@gmail.com>
2016-12-13 15:58:20 +0300

    Создала каталог Services, в нем интерфейсы IOpenProjectFileService и ISaveProjectFileService.

---
+++ MoneyCounter/MoneyCounter/Services/ISaveProjectFileService.cs
@@ -0,0 +1,15 @@
+﻿namespace MoneyCounter.Services
+{
+	/// <summary>
+	/// Интерфейс сохранения файла.
+	/// </summary>
+	public interface ISaveProjectFileService
+	{
+		/// <summary>
+		/// Открывает дилог выбора файла.
+		/// </summary>
+		/// <param name="path">Возвращает путь к файлу</param>
+		/// <returns>Истина - пользовавтель выбрал файл. Ложь - отмена выбора.</returns>
+		bool? SaveProjectFile(out string path);
+	}
+}
