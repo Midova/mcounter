0f13e197e41306a08d0093e3493bf208fc0d6a8f
Midova <darya.demidova2011@gmail.com>
2016-12-13 16:27:54 +0300

    Создала классы: OpenProjectFileService и SaveProjectFileService

---
+++ MoneyCounter/MoneyCounter/Services/OpenProjectFileService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Win32;
+
+namespace MoneyCounter.Services
+{
+	public class OpenProjectFileService : IOpenProjectFileService
+	{
+		public bool? OpenProjectFile(out string path)
+		{
+			var dialog = new OpenFileDialog()
+			{				
+				Multiselect = false,
+				DefaultExt = ".budget",
+				Filter = "Budget file|*.budget"
+			};
+
+			var result = dialog.ShowDialog();
+			path = dialog.FileName;
+
+			return result;
+		}
+	}
+}
---
+++ MoneyCounter/MoneyCounter/Services/SaveProjectFileService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyCounter.Services
+{
+	public class SaveProjectFileService
+	{
+		public bool? SaveProjectFile(out string path)
+		{
+			var dialog = new SaveFileDialog()
+			{
+				DefaultExt = ".budget"
+			};
+
+			var result = dialog.ShowDialog();
+			path = dialog.FileName;
+
+			return result;
+		}
+	}
+}
