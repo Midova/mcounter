4fe5d775b9ed23169245b60120bfa719f92f0376
Midova <darya.demidova2011@gmail.com>
2016-12-15 15:59:26 +0300

    Дополнила имеющиеся юнит тесты. Оттестировала. Изменила разрешение файла. Дописала коментарии.

--- MoneyCounter/MoneyCounter/Services/SaveProjectFileService.cs
+++ MoneyCounter/MoneyCounter/Services/SaveProjectFileService.cs
@@ -1,19 +1,22 @@
 ﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MoneyCounter.Services
 {
+	/// <summary>
+	/// Сервис сохранения в файл.
+	/// </summary>
 	public class SaveProjectFileService
 	{
+		/// <summary>
+		/// Открывает дилог выбора файла.
+		/// </summary>
+		/// <param name="path">Возвращает путь к файлу</param>
+		/// <returns>Истина - пользовавтель выбрал файл. Ложь - отмена выбора.</returns>
 		public bool? SaveProjectFile(out string path)
 		{
 			var dialog = new SaveFileDialog()
 			{
-				DefaultExt = ".budget"
+				DefaultExt = ".mcounter"
 			};
 
 			var result = dialog.ShowDialog();
