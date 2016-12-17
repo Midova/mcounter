849f9d00b0103182fb1817c6b5df9307902b15c6
Midova <darya.demidova2011@gmail.com>
2016-12-17 12:13:48 +0300

    В модели представлеиня главного окна добавила обработчики и соотвествующие им команды для поддержки сервиса сохранения загрузки проекта ( работы с пунктами меню Загрузить проект, Сохранить проект и Сохранить как). Добавленно св- во Session. Добавила команду закытия сессии( заготовка)

--- MoneyCounter/MoneyCounter/MainViewModel.cs
+++ MoneyCounter/MoneyCounter/MainViewModel.cs
@@ -1,12 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Catel.MVVM;
+using MoneyCounter.Services;
+using System.Windows.Input;
 
 namespace MoneyCounter
 {
 	public class MainViewModel
-	{
+	{		
+		public MainViewModel(IOpenProjectFileService fileOpenDialogService,
+			ISaveProjectFileService fileSaveDialogService)
+		{
+			_FileOpenDialogService = fileOpenDialogService;
+			_FileSaveDialogService = fileSaveDialogService;
+
+			LoadSessionCommand = new Command(LoadSession);
+			SaveSessionCommand = new Command(SaveSession);
+			SaveAsSessionCommand = new Command(SaveAsSession);
+		}
+
+		/// <summary>
+		/// Сервис загрузки данных из файла.
+		/// </summary>
+		private IOpenProjectFileService _FileOpenDialogService;
+
+		/// <summary>
+		/// Сервис сохранения данных в файл.
+		/// </summary>
+		private ISaveProjectFileService _FileSaveDialogService;
+
+		/// <summary>
+		/// Текущая загруженная сессия.
+		/// </summary>
+		public Session Session { get; private set; }
+
+
+		/// <summary>
+		/// Получает обработчик загрузки проекта из файла.
+		/// </summary>
+		public ICommand LoadSessionCommand { get; private set; }
+
+		/// <summary>
+		/// Загрузка проекта из файла.
+		/// </summary>
+		private void LoadSession()
+		{
+
+		}
+
+		/// <summary>
+		/// Получает обработчик сохранения проекта в файл.
+		/// </summary>
+		public ICommand SaveSessionCommand { get; private set; }
+
+		/// <summary>
+		/// Сохранение проекта в файл.
+		/// </summary>
+		private void SaveSession()
+		{
+
+		}
+
+		/// <summary>
+		/// Получает обработчик сохранения проекта в файл по новому адресу.
+		/// </summary>
+		public ICommand SaveAsSessionCommand { get; private set; }
+
+		/// <summary>
+		/// Cохранение проекта в файл по новому адресу.
+		/// </summary>
+		private void SaveAsSession()
+		{
+
+		}
+
+		/// <summary>
+		/// Получает обработчик закрытия сессии.
+		/// </summary>
+		public ICommand ClosingSessionCommand { get; private set; }
 	}
 }
