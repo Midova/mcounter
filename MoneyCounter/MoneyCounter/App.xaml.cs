31128a9f1bde94cb499e95ba0881437342f89a59
Midova <darya.demidova2011@gmail.com>
2017-02-16 15:29:39 +0300

    В окне OperationTemplateEditorView сдела разметку. Добавила картинку add.png В окне OperationTemplateView сдела рамку.

--- MoneyCounter/MoneyCounter/App.xaml.cs
+++ MoneyCounter/MoneyCounter/App.xaml.cs
@@ -29,7 +29,7 @@ namespace MoneyCounter
 			MainWindow = mainWindow;
 			MainWindow.Show();
 
-			var Tem = new OperationTemplateView();
+			var Tem = new OperationTemplateEditorView();
 
 			Tem.Show();
 		}
